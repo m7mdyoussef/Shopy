5307bf55f5a6063002b4f11d0c7594e517e2ad7c
SohaMohamed98 <48870254+SohaMohamed98@users.noreply.github.com>
2021-05-31 21:24:27 -0700

    designCell

---
+++ Shopy/Utils/AppCommon.swift
@@ -0,0 +1,30 @@
+//
+//  App Common.swift
+//  Shopy
+//
+//  Created by SOHA on 5/31/21.
+//  Copyright © 2021 mohamed youssef. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+    
+    func circular() {
+        self.layer.cornerRadius = self.frame.height/2
+    }
+    
+    func collectionCellLayout(){
+        self.layer.cornerRadius = 20
+        self.layer.borderWidth = 0.5
+        self.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
+}
+
