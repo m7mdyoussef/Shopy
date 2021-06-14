406f5527384500e9830f8dc6cf571c19a69985a5
SohaMohamed98 <48870254+SohaMohamed98@users.noreply.github.com>
2021-06-14 04:51:49 -0700

    add missing file

---
+++ Shopy/Utils/ErrorMessage.swift
@@ -0,0 +1,22 @@
+//
+//  ErrorMessage.swift
+//  Shopy
+//
+//  Created by Mahmoud Elattar on 20/05/2021.
+//  Copyright © 2021 mohamed youssef. All rights reserved.
+//
+
+import Foundation
+
+struct ErrorMessage {
+    static let genericError = "Something went wrong please try again later"
+}
+/*
+enum ErrorMessage: String ,Error{
+    case invalidUsername    = "This username created an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again."
+    
+}
+*/
