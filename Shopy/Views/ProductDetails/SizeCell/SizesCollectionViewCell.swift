2a2fafc22b0c2b7c6824a9b30e2df01e910e8db3
SohaMohamed98 <48870254+SohaMohamed98@users.noreply.github.com>
2021-06-21 09:59:45 -0700

    Size Cell

--- Shopy/Views/ProductDetails/SizeCell/SizesCollectionViewCell.swift
+++ Shopy/Views/ProductDetails/SizeCell/SizesCollectionViewCell.swift
@@ -23,7 +23,7 @@ class SizesCollectionViewCell: UICollectionViewCell {
     override var isSelected: Bool{
         didSet{
             productSize.textColor = isSelected ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
-            productSize.layer.backgroundColor = isSelected ? #colorLiteral(red: 0.9631432891, green: 0.6232308745, blue: 0.0003859905701, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            productSize.layer.backgroundColor = isSelected ? #colorLiteral(red: 0.9631432891, green: 0.6232308745, blue: 0.0003859905701, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
 
         }
     }
@@ -35,6 +35,7 @@ class SizesCollectionViewCell: UICollectionViewCell {
         productSize.layer.borderWidth = 0.5
         productSize.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         productSize.layer.cornerRadius = 8
+        productSize.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 
 }
